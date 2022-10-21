f43d415ec384f4f9f9a08d27a457117997aa72a5
therealyo <dpshenik2002@gmail.com>
2022-10-21 22:31:35 +0300

    add orders service

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,73 +1,88 @@
-import { Knex } from 'knex';
+import { Knex } from 'knex'
 
 import { BaseService } from '../types/BaseService'
 
 export class OrderService extends BaseService {
-
-
     constructor(db: Knex) {
         super(db)
     }
 
-   
-    // private readonly mapOrderInfo = (
-    //     orderInfo: JoinBuilderResponses<
-    //         [ExtractModel<OrdersTable>, ExtractModel<OrderDetailsTable>, ExtractModel<ShippersTable>]
-    //     >
-    // ) => {
-    //     return orderInfo
-    //         .map((order, detail, shipper) => {
-    //             // form array of joined order info with separate total price for each order
-    //             return {
-    //                 ...order,
-    //                 TotalPrice: detail.UnitPrice! * detail.Quantity!,
-    //                 ShipVia: shipper.CompanyName
-    //             }
-    //         })
-    //         .reduce((order, anotherOrder) => ({
-    //             // count total price of all orders
-    //             ...order,
-    //             TotalPrice: order.TotalPrice + anotherOrder.TotalPrice
-    //         }))
-    // }
-
-    // private readonly mapProductsInfo = (
-    //     productsInfo: JoinBuilderResponses<[ExtractModel<ProductsTable>, ExtractModel<OrderDetailsTable>]>
-    // ) => {
-    //     return productsInfo.map((product, detail) => {
-    //         return {
-    //             ...product,
-    //             OrderID: detail.OrderID,
-    //             Quantity: detail.Quantity,
-    //             OrderPrice: detail.UnitPrice,
-    //             TotalPrice: detail.Quantity! * detail.UnitPrice!,
-    //             Discount: detail.Discount
-    //         }
-    //     })
-    // }
-
     getOrderInfo = async (id: number) => {
-        // const orderInfo = await this.ordersTable!.select()
-        //     .leftJoin(this.detailsTable!, (orders, details) => eq(orders.OrderID, details.OrderID))
-        //     .leftJoin(this.shippersTable!, (orders, details, shippers) => eq(orders.ShipVia, shippers.ShipperID))
-        //     .where((orders, details) => eq(orders.OrderID, id))
-        //     .execute()
+        const orderQuery = this.db
+            .queryBuilder()
+            .select(
+                this.db.raw('sum(?? * ??) as ??', ['orderdetails.UnitPrice', 'orderdetails.Quantity', 'TotalPrice']),
+                'orders.OrderID',
+                'orders.OrderID',
+                'orders.OrderDate',
+                'orders.RequiredDate',
+                'orders.ShippedDate',
+                'shippers.CompanyName as ShipVia',
+                'orders.Freight',
+                'orders.ShipName',
+                'orders.ShipAddress',
+                'orders.ShipCity',
+                'orders.ShipCountry'
+            )
+            .from('orders')
+            .leftJoin('orderdetails', 'orders.OrderID', 'orderdetails.OrderID')
+            .leftJoin('shippers', 'orders.ShipVia', 'shippers.ShipperID')
+            .groupBy('orders.OrderID')
+            .groupBy('shippers.CompanyName')
+            .where('orders.OrderID', '=', id)
 
-        // const productsInfo = await this.productsTable!.select()
-        //     .leftJoin(this.detailsTable!, (products, details) => eq(products.ProductID, details.ProductID))
-        //     .where((products, details) => eq(details.OrderID, id))
-        //     .execute()
+        this.logger.addQuery(orderQuery.toQuery())
+        const orderInfo = await orderQuery.first()
 
-        // return {
-        //     queries: this.logger.retrieveQueries(),
-        //     data: {
-        //         ...this.mapOrderInfo(orderInfo),
-        //         Products: this.mapProductsInfo(productsInfo)
-        //     }
-        // }
+        const productsQuery = this.db
+            .queryBuilder()
+            .select()
+            .from('products')
+            .leftJoin('orderdetails', 'products.ProductID', 'orderdetails.ProductID')
+            .groupBy('products.ProductID')
+            .groupBy('orderdetails.id')
+            .groupBy('orderdetails.OrderID')
+            .where('orderdetails.OrderID', '=', id)
+        this.logger.addQuery(productsQuery.toQuery())
+        const products = await productsQuery
+
+        return {
+            queries: this.logger.retrieveQueries(),
+            data: { ...orderInfo, Products: products }
+        }
     }
 
     getOrdersPage = async (page: number) => {
-    //    
+        const countQuery = this.db.queryBuilder().select().from('orders').count()
+
+        this.logger.addQuery(countQuery.toQuery())
+        const { count } = await countQuery.first()
+
+        const pageQuery = this.db('orders')
+            .leftJoin('orderdetails', 'orders.OrderID', 'orderdetails.OrderID')
+            .select(
+                this.db.raw('sum(?? * ??) as ??', ['orderdetails.UnitPrice', 'orderdetails.Quantity', 'TotalPrice']),
+                this.db.raw('sum(??) as ??', ['orderdetails.Quantity', 'TotalQuantity']),
+                this.db.raw('COUNT(??) as ??', ['orderdetails.OrderID', 'TotalProducts']),
+                'orders.OrderID',
+                'orders.OrderID',
+                'orders.OrderDate',
+                'orders.RequiredDate',
+                'orders.ShippedDate',
+                'orders.ShipVia',
+                'orders.Freight',
+                'orders.ShipName',
+                'orders.ShipAddress',
+                'orders.ShipCity',
+                'orders.ShipCountry'
+            )
+            .groupBy('orders.OrderID')
+            .limit(this.pageSize)
+            .offset(this.pageSize * (page - 1))
+
+        this.logger.addQuery(pageQuery.toQuery())
+        const pageData = await pageQuery
+
+        return { queries: this.logger.retrieveQueries(), count, page: pageData }
     }
 }
